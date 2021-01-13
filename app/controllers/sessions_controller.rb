6600af558d1385ba0c71d5f92418259806c8b7d3
jbrasko <jbrasko0508@gmail.com>
2021-01-13 11:59:34 -0600

    commit tree 36e3686b254ebb1e73c9f9f852b01692e4d80d8a

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user = User.find_by(name: params[:user][:name])
+        user = user.try(:authenticate, params[:user][:password])
+
+        if user
+            session[:user_id] = user.id
+
+            @user = user
+            redirect_to controller: 'users', action: 'home'
+        else
+            redirect_to '/new'
+        end
+    end
+    
+end
